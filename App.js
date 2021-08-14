c8da768a5b52d02fba3a5e46b6751bb90fc059d8
smarthubvn <smarthubvn@gmail.com>
2021-08-14 08:08:26 +0700

    Scrollview

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoryListItem from './components/CategoryListItem';
-import SkiImage from './assets/ski.png';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, ScrollView, View } from "react-native";
+import CategoryListItem from "./components/CategoryListItem";
+import SkiImage from "./assets/ski.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CategoryListItem title="Trượt tuyết" image={SkiImage}/>
-      <CategoryListItem title="Trượt tuyết" image={SkiImage}/>
-      <CategoryListItem title="Trượt tuyết" image={SkiImage}/>
+      <ScrollView>
+        <CategoryListItem title="Trượt tuyết" image={SkiImage} />
+        <CategoryListItem title="Trượt tuyết" image={SkiImage} />
+        <CategoryListItem title="Trượt tuyết" image={SkiImage} />
+        <CategoryListItem title="Trượt tuyết" image={SkiImage} />
+        <CategoryListItem title="Trượt tuyết" image={SkiImage} />
+        <CategoryListItem title="Trượt tuyết" image={SkiImage} />
+        <CategoryListItem title="Trượt tuyết" image={SkiImage} />
+        <CategoryListItem title="Trượt tuyết" image={SkiImage} />
+      </ScrollView>
     </View>
   );
 }
@@ -17,10 +24,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'stretch',
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    alignItems: "stretch",
+    backgroundColor: "#fff",
+    justifyContent: "center",
     paddingLeft: 16,
-    paddingRight: 16
+    paddingRight: 16,
   },
 });
